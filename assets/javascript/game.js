4e6e3ad337d3cb488bc155133d93fcc13565134d
cm718 <cm718@icloud.com>
2018-10-17 23:27:45 -0500

    Added comments to js. Game is working, but needs a few more things.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,8 +3,6 @@
 $(document).ready(function() {
 
   // Declaring my variables at the top of the page
-  var gameOver = false;
-  var winGame = false;
   var wins = 0;
   var guess = "";
   var answerArray = [];
@@ -26,56 +24,74 @@ $(document).ready(function() {
   var word = "";
   var guessedLetters = [];
 
-  //*******************************************
-  // setting the logic for the game
-  //*******************************************
 
+// ************************************************************
 
-  //setting the users guess to their key click
-  //and transforming it to upper case
-  $(this).on("keypress", function(event) {
+  // Setting the users guess to their key click
+  $(this).on("keypress", function() {
+    //transforming it to upper case
     guess = event.key.toUpperCase();
+    // Runs the function guessLetter();
     guessLetter();
+    // Pushes their guess into the array called guess
     guessedLetters.push(guess);
+    // Changes the html to display their guesses.
     $("#guessedLetters").text(guessedLetters.join(", "));
-    if (answerArray.toString().replace(/,/g,"") == word) {
+    // Checks to see if answerArray is equal to the chosen word.
+    if (answerArray.toString().replace(/,/g, "") == word) {
+      // If the user wins increase the win score
       wins = wins + 1;
-      winGame === true;
       $("#wins").text(wins);
-      alert("You win! Want to play again?")
-  }
+      $(".winner").css("display", "block");
+    }
   });
 
-  $("#playAgain").on("click", newGame);
+// ************************************************************
 
-  // looping through the chosen word
-  // and making an array of underscores
+  // Declaring a function called guessLetter
+  function guessLetter() {
+    // This loops through the word to see if the letter chosen
+    for (var j = 0; j < word.length; j++) {
+      // matches any of the letters in the "word" varialbe.
+      if (word[j] === guess) {
+        answerArray[j] = guess;
+      } 
+    }
+    // Adds the correct letter at the correct index of the array
+    $("#answerArray").text(answerArray.join(" "));
+  }
+
+// ************************************************************
+
+  // Declare a function to call when I want the game to reset
   function newGame() {
+    // Hide the button and the winner text
+    $(".winner").css("display", "none");
+    // For each new game I want to reset the array of user guessed letters
     guessedLetters = [];
+    // Reset the html for that array of letters as well.
     $("#guessedLetters").empty();
+    // Reset the game logic for the answer array
     answerArray = [];
+    // And reset the html for the answer array to an empty string.
     $("#answerArray").text("");
+    // Then it chooses a random word from the words array
     word = words[Math.floor(Math.random() * words.length)];
+    // Then loops through the word and creates underscores for each letter
     for (var i = 0; i < word.length; i++) {
       answerArray[i] = "_";
     }
     console.log(word);
+    // Then changes the html to add an underscore and space for each letter
     $("#answerArray").text(answerArray.join(" "));
   }
 
-  // Declaring a function called guessLetter
-  function guessLetter() {
-    for (var j = 0; j < word.length; j++) {
-      if (word[j] === guess) {
-        answerArray[j] = guess;
-      }
-    }
-    $("#answerArray").text(answerArray.join(" "));
-  }
+  // ************************************************************
+
+  // Setting the instructions for the Play Again button to run newGame();
+  $("#playAgain").on("click", newGame);
 
   // calling the newGame function so the game starts
-  // and guessLetter so that it prints the underscores
   newGame();
-
 
 });
